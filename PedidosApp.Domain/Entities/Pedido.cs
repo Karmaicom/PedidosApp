023a29826f7cb2a561d6b37a1d0d68b05e7a381c
karmaicom <karmaicom@gmail.com>
2025-06-06 22:06:10 -0300

    feat: Classe Pedido criada; Docker-compose criado.

---
+++ PedidosApp.Domain/Entities/Pedido.cs
@@ -0,0 +1,20 @@
+﻿namespace PedidosApp.Domain.Entities
+{
+    public class Pedido
+    {
+        public required Guid Id { get; set; }
+        public required string Solicitante { get; set; }
+        public required DateTime DataHora { get; set; }
+        public required decimal Valor { get; set; }
+        public required string Detalhes { get; set; }
+        public required Status Status { get; set; }
+        public required bool Ativo { get; set; }
+    }
+
+    public enum Status
+    {
+        Pendente = 1,
+        Aprovado = 2,
+        Rejeitado = 3
+    }
+}
